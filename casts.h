b313c0af9e9ec8a15f2cb43f05f3ba8f233da444
netoe1 <elynetobr@outlook.com>
2024-11-13 16:27:01 -0300

    creating

---
+++ casts.h
@@ -0,0 +1,23 @@
+#ifndef CASTS_H
+#define CASTS_H
+
+#define DATATYPE
+#define setData(x)
+
+
+#pragma region varsCast  // Creating casts for comum types.
+#define void_cast (void)
+#define char_cast (char)
+#define int_cast (int)
+#define float_cast (float)
+#define double_cast (double)
+#define long_cast (long)
+#pragma endregion varsCast
+#pragma region ptrCast
+#define voidptr_cast (void*)
+#define charptr_cast (char*)
+#define intptr_cast (int*)
+#define floatptr_cast (float*)
+#define doubleptr_cast (double*)
+#pragma endregion ptrCast
+#endif
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "casts.h"
+
+int main(void)
+{
+
+    setData("meu nome!");
+    printf("%s", DATATYPE);
+    return EXIT_SUCCESS;
+}
